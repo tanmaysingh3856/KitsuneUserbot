1410ea209040ae8012d3af4078223629aaef5988
Tanmay Singh <89771502+tanmaysingh3856@users.noreply.github.com>
2023-06-30 19:05:57 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,31 @@
+from os import getenv
+
+from dotenv import load_dotenv
+
+load_dotenv("config.env")
+
+
+API_HASH = getenv("API_HASH")
+API_ID = int(getenv("API_ID", ""))
+BLACKLIST_CHAT = getenv("BLACKLIST_CHAT", None)
+BLACKLIST_GCAST = {int(x) for x in getenv("BLACKLIST_GCAST", "").split()}
+BOTLOG_CHATID = int(getenv("BOTLOG_CHATID") or 0)
+PMLOG_CHATID = int(getenv("PMLOG_CHATID") or 0)
+BOT_VER = "0.1.0"
+BRANCH = getenv("BRANCH", "main")
+CHANNEL = getenv("CHANNEL", "Pixsuvy")
+CMD_HANDLER = getenv("CMD_HANDLER", ".")
+CMD_HNDLR = CMD_HANDLER
+DB_URL = getenv("DATABASE_URL", "")
+GIT_TOKEN = getenv("GIT_TOKEN", "")
+GROUP = getenv("GROUP", "Pixsuvy")
+HEROKU_API_KEY = getenv("HEROKU_API_KEY", None)
+HEROKU_APP_NAME = getenv("HEROKU_APP_NAME", None)
+PMPERMIT_PIC = getenv("PMPERMIT_PIC", None)
+REPO_URL = getenv("REPO_URL", "https://github.com/Pixsuvy/PixsuvyUserbot")
+STRING_SESSION1 = getenv("STRING_SESSION1", "")
+STRING_SESSION2 = getenv("STRING_SESSION2", "")
+STRING_SESSION3 = getenv("STRING_SESSION3", "")
+STRING_SESSION4 = getenv("STRING_SESSION4", "")
+STRING_SESSION5 = getenv("STRING_SESSION5", "")
+SUDO_USERS = list(map(int, getenv("SUDO_USERS", "").split()))
