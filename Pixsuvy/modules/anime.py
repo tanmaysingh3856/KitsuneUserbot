c0772a034646defcf94d06ff03f9d85196857f04
Tanmay Singh <89771502+tanmaysingh3856@users.noreply.github.com>
2023-10-21 00:56:10 +0530

    gg

---
+++ Pixsuvy/modules/anime.py
@@ -0,0 +1,35 @@
+import requests
+from pyrogram import filters, Client
+from pyrogram.types import Message
+from .help import add_command_help
+from config import CMD_HANDLER as cmd
+
+API = "https://api.animecyberspace.com/v1/anime"
+
+@Client.on_message(filters.command("anime", cmd) & filters.me)
+async def anime(client: Client, message: Message):
+    if len(message.command) < 2:
+        await message.edit("`Give me a anime name`")
+        return
+    query = message.text.split(None, 1)[1]
+    try:
+        r = requests.get(f"{API}?q={query}").json()
+        anime = r["data"][0]
+        title = anime["title"]
+        description = anime["description"]
+        image = anime["image"]
+        await message.edit(f"**Title:** `{title}`\n\n**Description:** `{description}`", disable_web_page_preview=True)
+        await client.send_photo(message.chat.id, image, caption=f"**Title:** `{title}`\n\n**Description:** `{description}`")
+    except IndexError:
+        await message.edit("`No anime found`")
+        return
+    except Exception as e:
+        await message.edit(f"`{e}`")
+        return
+
+add_command_help(
+    "anime",
+    [
+        ["anime <anime name>", "Get information about a anime."],
+    ],
+)
