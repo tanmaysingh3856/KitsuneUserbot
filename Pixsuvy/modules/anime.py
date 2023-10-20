b4d8435f8491ecefc395610eed40de1a9d64ebf3
Tanmay Singh <89771502+tanmaysingh3856@users.noreply.github.com>
2023-10-21 01:00:51 +0530

    Update anime.py

--- Pixsuvy/modules/anime.py
+++ Pixsuvy/modules/anime.py
@@ -8,24 +8,18 @@ API = "https://api.nekosapi.com/v2/images/random"
 
 @Client.on_message(filters.command("anime", cmd) & filters.me)
 async def anime(client: Client, message: Message):
-    if len(message.command) < 2:
-        await message.edit("`Give me a anime name`")
-        return
-    query = message.text.split(None, 1)[1]
+    await message.edit("Fetching a random anime image...")
     try:
-        r = requests.get(f"{API}?q={query}").json()
-        anime = r["data"][0]
-        title = anime["title"]
-        description = anime["description"]
-        image = anime["image"]
-        await message.edit(f"**Title:** `{title}`\n\n**Description:** `{description}`", disable_web_page_preview=True)
-        await client.send_photo(message.chat.id, image, caption=f"**Title:** `{title}`\n\n**Description:** `{description}`")
-    except IndexError:
-        await message.edit("`No anime found`")
+        response = requests.get(API_URL)
+        response.raise_for_status()
+        data = response.json()["data"]["attributes"]
+        image_url = data["file"]
+        title = data["title"]
+    except (requests.exceptions.RequestException, KeyError):
+        await message.edit("Failed to fetch a random anime image.")
         return
-    except Exception as e:
-        await message.edit(f"`{e}`")
-        return
+    await client.send_photo(message.chat.id, image_url, caption=f"**Title:** {title}")
+    await message.edit("Random anime image sent!")
 
 add_command_help(
     "anime",
