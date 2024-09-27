c38492223ef18b5396c6d43ac5df8cf66bce1b23
Tanmay Singh <89771502+tanmaysingh3856@users.noreply.github.com>
2024-09-27 09:53:55 +0530

    testing

--- Kitsune/__main__.py
+++ Kitsune/__main__.py
@@ -1,9 +1,8 @@
 import importlib
-import tracemalloc  # Import tracemalloc
-
+import tracemalloc
+import asyncio
 from pyrogram import idle
 from uvloop import install
-
 from config import BOT_VER, CMD_HANDLER
 from Kitsune import BOTLOG_CHATID, LOGGER, LOOP, aiosession, bot1, bots
 from Kitsune.helpers.misc import create_botlog, heroku
@@ -15,31 +14,40 @@ PIX_ON = "Kitsune Userbot has been started"
 tracemalloc.start()
 
 async def main():
-    for all_module in ALL_MODULES:
-        importlib.import_module(f"Kitsune.modules.{all_module}")
-    for bot in bots:
-        try:
-            await bot.start()
-            bot.me = await bot.get_me()
+    try:
+        for all_module in ALL_MODULES:
+            importlib.import_module(f"Kitsune.modules.{all_module}")
+        for bot in bots:
             try:
-                await bot.send_message(
-                    BOTLOG_CHATID, PIX_ON.format(BOT_VER, CMD_HANDLER)
+                await bot.start()
+                bot.me = await bot.get_me()
+                try:
+                    await bot.send_message(
+                        BOTLOG_CHATID, PIX_ON.format(BOT_VER, CMD_HANDLER)
+                    )
+                except BaseException:
+                    pass
+                LOGGER("Kitsune").info(
+                    f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
                 )
-            except BaseException:
-                pass
-            LOGGER("Kitsune").info(
-                f"Logged in as {bot.me.first_name} | [ {bot.me.id} ]"
-            )
-        except Exception as a:
-            LOGGER("main").warning(a)
-    LOGGER("Kitsune").info(f"Kitsune v{BOT_VER}")
-    if bot1 and not str(BOTLOG_CHATID).startswith("-100"):
-        await create_botlog(bot1)
-    await idle()
-    await aiosession.close()
+            except Exception as a:
+                LOGGER("main").warning(a)
+        LOGGER("Kitsune").info(f"Kitsune v{BOT_VER}")
+        if bot1 and not str(BOTLOG_CHATID).startswith("-100"):
+            await create_botlog(bot1)
+        await idle()
+    except Exception as e:
+        LOGGER("main").warning(f"Exception in main: {e}")
+    finally:
+        await aiosession.close()
 
 if __name__ == "__main__":
     LOGGER("Kitsune").info("Starting Kitsune")
     install()
     heroku()
-    LOOP.run_until_complete(main())
+    try:
+        LOOP.run_until_complete(main())
+    except (KeyboardInterrupt, SystemExit):
+        LOGGER("main").info("Kitsune stopped")
+    finally:
+        LOOP.close()
